e9270c9e05384a9d78c00970f282070d67c5ea23
AramburuL <maramburu280@gmail.com>
2025-03-19 17:00:12 +0100

    GetByID

--- AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountController.java
+++ AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountController.java
@@ -1,11 +1,21 @@
 package com.microcompany.accountsservice.controller;
 
+import com.microcompany.accountsservice.model.Account;
+import com.microcompany.accountsservice.persistence.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController implements  AccountControllerInterface{
+    @Autowired
+    AccountRepository repo;
     @Override
     public String testMapping() {
         return "Mapping funciona";
+    }
+
+    @Override
+    public Account getCuentaById(long cid) {
+        return repo.getById(cid);
     }
 }
--- AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountControllerInterface.java
+++ AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountControllerInterface.java
@@ -1,5 +1,8 @@
 package com.microcompany.accountsservice.controller;
 
+import com.microcompany.accountsservice.model.Account;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public interface AccountControllerInterface {
     @RequestMapping("")
     public String testMapping();
+
+    @RequestMapping("/{id}")
+    public Account getCuentaById(@PathVariable("id") long cid);
 }
