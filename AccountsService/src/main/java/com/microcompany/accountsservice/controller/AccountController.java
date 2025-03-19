10a3a108874c633f49b5701b8609e1cea4b9bef1
AramburuL <maramburu280@gmail.com>
2025-03-19 16:33:49 +0100

    Controller test

--- AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountController.java
+++ AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountController.java
@@ -1,4 +1,11 @@
 package com.microcompany.accountsservice.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController implements  AccountControllerInterface{
+    @Override
+    public String testMapping() {
+        return "Mapping funciona";
+    }
 }
--- AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountControllerInterface.java
+++ AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountControllerInterface.java
@@ -1,4 +1,11 @@
 package com.microcompany.accountsservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cuentas")
 public interface AccountControllerInterface {
+    @RequestMapping("")
+    public String testMapping();
 }
