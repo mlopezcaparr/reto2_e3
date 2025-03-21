a90824729a9a8d351ece1c8c15fbc7145502dd06
AramburuL <maramburu280@gmail.com>
2025-03-21 13:25:47 +0100

    https en perfil prod

---
+++ AccountsService/src/main/java/com/microcompany/accountsservice/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.microcompany.accountsservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer  corsConfigures(){
+        return new WebMvcConfigurer() {
+            public void addCorsMapping(CorsRegistry registry){
+                registry.addMapping("/cuentas").allowedOrigins("*");
+            }
+        };
+    }
+}
