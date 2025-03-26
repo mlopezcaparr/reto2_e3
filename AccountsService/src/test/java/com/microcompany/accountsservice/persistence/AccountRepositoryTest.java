e84eea043a56db1db19d5286b6e6b9e63e256bc3
AramburuL <maramburu280@gmail.com>
2025-03-26 12:23:58 +0100

    Test repo

--- AccountsService/src/test/java/com/microcompany/accountsservice/persistence/AccountRepositoryTest.java
+++ AccountsService/src/test/java/com/microcompany/accountsservice/persistence/AccountRepositoryTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.security.auth.login.AccountException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,14 +25,16 @@ class AccountRepositoryTest {
     }
 
     @Test
-    void findByOwnerId() {
+    void givenValidId_whenFindByOwnerId_thenReturnAccount() {
         List<Account> acs = repo.findByOwnerId(2L);
         assertEquals(acs.get(1).getOwnerId(), 2);
         assertTrue(true);
     }
 
+    
+
     @Test
-    void getSumAccounts() {
+    void givenValidId_whenGetSumAccounts_thenReturnSum() {
         List<Account> acs = repo.findByOwnerId(2L);
         int balance = 0;
         for(Account a : acs){
