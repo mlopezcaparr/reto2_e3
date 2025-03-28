e7fec643874f51bd2d72a6a237fda89c864e0a38
unknown <gabrieleduardosilvalopez@gmail.com>
2025-03-28 01:12:43 +0100

    reto2part1

--- AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountControllerInterface.java
+++ AccountsService/src/main/java/com/microcompany/accountsservice/controller/AccountControllerInterface.java
@@ -1,10 +1,6 @@
 package com.microcompany.accountsservice.controller;
 
 import com.microcompany.accountsservice.model.Account;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,117 +11,43 @@ import javax.validation.constraints.Min;
 @RestController
 @RequestMapping(value = "/accounts", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 @Validated
-@Tag(name = "Endpoints Para API de Accounts", description = "Endpoints para la gestión de cuentas de los usuarios")
 public interface AccountControllerInterface {
 
-
-    @Operation(summary = "Método pedir una cuenta del usuario", description = "Método para pedir una cuenta de un cliente" +
-            " pasando el id del cliente y el id de la cuenta, devuelve un JSON o XML.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Si existe, devolverá como resultado la cuenta."),
-            @ApiResponse(responseCode = "404", description = "Si no existen, devolverá un mensaje de que no la encontrado."),
-            @ApiResponse(responseCode = "412", description = "Devolvera este error en caso de pasarle un parametro erróneo."),
-    })
     @RequestMapping(value = "/{uid}", method = RequestMethod.GET)
     public ResponseEntity getCuentaById(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") long uid,
                                         @Min(value = 1, message = "Account id must be 1 or higher") @RequestParam long cid);
 
-    @Operation(summary = "Método para pedir cuentas de un usuario", description = "Método que solicita las cuentas" +
-            " de un cliente pasando el id del cliente y delvuelve una lista de cuentas, devuelve un JSON o XML.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Si existe, devolverá como resultado la cuenta/las cuentas."),
-            @ApiResponse(responseCode = "404", description = "Si no existen, devolverá un mensaje de que no la encontrado."),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @RequestMapping(value = "/{uid}/all", method = RequestMethod.GET)
     public ResponseEntity getUserCuentas(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") long uid);
 
-    @Operation(summary = "Método para validar préstamo de un usuario", description = "Método que valida si un usuario" +
-            " puede extraer 'x' cantidad de dinero de sus cuentas, devuelve un JSON o XML.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Si es valido, devolverá como resultado un mensaje de true."),
-            @ApiResponse(responseCode = "404", description = "Si no es valido, devolverá como resultado un mensaje de false."),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @RequestMapping(value = "/{uid}/validate", method = RequestMethod.GET)
     public ResponseEntity validateCuenta(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") long uid,
                                          @Min(value = 0, message = "The amount of money must be higher 0 or higher")
                                          @RequestParam("cantidad") int cant);
 
-    @Operation(summary = "Método para añadir a una cuenta", description = "Método que permite a un usuario agregar dinero" +
-            " a una cuenta, si el método se ha ejecutado correctamente no devolverá contenido, si fallá devuelve" +
-            " un JSON o XML con los errores.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Se ha agregado la cantidad correctamente sin errores."),
-            @ApiResponse(responseCode = "404", description = "Hubo un problema, no se puedo encontrar la cuenta o el usuario"),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @PutMapping(value = "/{uid}/add")
     ResponseEntity addToAccount(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") Long uid,
                                 @Min(value = 1, message = "Account id must be 1 or higher") @RequestParam(value = "cid") Long cid,
                                 @Min(value = 0, message = "The amount of money must be higher 0 or higher")
                                 @RequestParam(value = "cantidad", defaultValue = "") int cant);
 
-    @Operation(summary = "Método para extraer de cuentas", description = "Método que permite a un usuario extraer dinero" +
-            " de sus cuentas, si el método se ha ejecutado correctamente no devolverá contenido, si fallá devuelve" +
-            " un JSON o XML con los errores.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Se ha agregado la cantidad correctamente sin errores."),
-            @ApiResponse(responseCode = "400", description = "No se pudo extraer la cantidad porque superá el montón permitido"),
-            @ApiResponse(responseCode = "404", description = "Hubo un problema, no se puedo encontrar la cuenta o el usuario"),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @PutMapping(value = "/{uid}/withdraw")
     ResponseEntity removeFromAccount(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") Long uid,
                                      @Min(value = 1, message = "Account id must be 1 or higher") @RequestParam(value = "cid") Long cid,
                                      @Min(value = 0, message = "The amount of money must be higher 0 or higher")
                                      @RequestParam(value = "cantidad", defaultValue = "") int cant);
 
-    @Operation(summary = "Método para actualizar cuentas", description = "Método que permite a actualizar la cuenta de" +
-            " un usuario pasando el id de dicha cuenta por parametros, si el método se ha ejecutado correctamente no" +
-            " devolverá contenido, si fallá devuelve un JSON o XML con los errores.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Se ha agregado la cantidad correctamente sin errores."),
-            @ApiResponse(responseCode = "400", description = "La cuenta se paso como parametro de JSON vacío."),
-            @ApiResponse(responseCode = "404", description = "Hubo un problema, no se puedo encontrar la cuenta asociada al id"),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @PutMapping(value = "/{cid}", consumes = {MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity updateAccount(@Min(value = 1, message = "Account id must be 1 or higher") @PathVariable("cid") Long cid,
                                  @Valid @RequestBody Account account);
 
-    @Operation(summary = "Método para crear cuenta", description = "Método que permite crear una cuenta para" +
-            " un usuario pasando el id de dicha cuenta por parametros, si el método se ha ejecutado correctamente no" +
-            " devolverá contenido, si fallá devuelve un JSON o XML con los errores.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Se creado la cuenta correctamente sin errores."),
-            @ApiResponse(responseCode = "400", description = "Devolverá este error si no puede crear una cuenta con el balance"),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity createAccount(@Valid @RequestBody Account account);
 
-    @Operation(summary = "Método para borrar cuenta", description = "Método que permite borrar una cuenta para" +
-            " un usuario pasando el id de dicha cuenta y el del usuario por parametros, si el método se ha ejecutado " +
-            " correctamente no devolverá contenido, si fallá devuelve un JSON o XML con los errores.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Se borra la cuenta correctamente sin errores."),
-            @ApiResponse(responseCode = "400", description = "Devolverá este error si el usuario no es propietario."),
-            @ApiResponse(responseCode = "404", description = "Devolverá este error si no puedo encontrar una cuenta."),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @DeleteMapping("/{uid}")
     ResponseEntity deleteAccount(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") Long uid,
                                  @Min(value = 1, message = "Account id must be 1 or higher") @RequestParam(value = "cid") Long cid);
 
-    @Operation(summary = "Método para borrar cuentas de usuario", description = "Método que permite borrar las cuentas de" +
-            " un usuario pasando el id del usuario por parametros, si el método se ha ejecutado " +
-            " correctamente no devolverá contenido, si fallá devuelve un JSON o XML con los errores.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Se borrán la cuenta correctamente sin errores."),
-            @ApiResponse(responseCode = "404", description = "Devolverá este error si no encontró ninguna cuenta asociada al usuario."),
-            @ApiResponse(responseCode = "412", description = "Devolverá este error en caso de pasarle un parametro erróneo."),
-    })
     @DeleteMapping("/{uid}/all")
     ResponseEntity deleteUserAccounts(@Min(value = 1, message = "Owner id must be 1 or higher") @PathVariable("uid") Long uid);
 }
